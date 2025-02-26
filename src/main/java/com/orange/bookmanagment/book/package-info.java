2f08ec18e92a729025953387b52b168c62aef556
CzarnaWoda <kmimat299@gmail.com>
2025-02-26 18:35:05 +0100

    Add ApplicationModule information

---
+++ src/main/java/com/orange/bookmanagment/book/package-info.java
@@ -0,0 +1,8 @@
+@ApplicationModule(
+        type = ApplicationModule.Type.CLOSED,
+        allowedDependencies = {"shared"}
+)
+
+package com.orange.bookmanagment.book;
+
+import org.springframework.modulith.ApplicationModule;
