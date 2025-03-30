0b14ed679f13f68248fc93decab0cb0c4edd8889
CzarnaWoda <kmimat299@gmail.com>
2025-03-30 13:48:07 +0200

    Notificaton models, user model changes (settings added)

---
+++ src/main/java/com/orange/bookmanagment/user/model/Settings.java
@@ -0,0 +1,38 @@
+package com.orange.bookmanagment.user.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "userSettings")
+@Table(name = "userSettings")
+
+@NoArgsConstructor
+@Data
+public class Settings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private boolean darkMode;
+
+    private boolean notificationsEnabled;
+
+    private boolean emailNotificationsEnabled;
+
+    private boolean emailRemindersEnabled;
+
+    private boolean newBooksNotificationsEnabled;
+
+    @OneToOne(mappedBy = "userSettings")
+    private User user;
+
+    public Settings(boolean darkMode, boolean notificationsEnabled, boolean emailNotificationsEnabled, boolean emailRemindersEnabled, boolean newBooksNotificationsEnabled) {
+        this.darkMode = darkMode;
+        this.notificationsEnabled = notificationsEnabled;
+        this.emailNotificationsEnabled = emailNotificationsEnabled;
+        this.emailRemindersEnabled = emailRemindersEnabled;
+        this.newBooksNotificationsEnabled = newBooksNotificationsEnabled;
+    }
+}
