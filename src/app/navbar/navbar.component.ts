0afb544e3d27efd23f108392c3981a50bfa1430d
PiotrasX <piotrus.rojek@wp.pl>
2025-05-06 22:28:16 +0200

    Dodanie ciemnego motywu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   isLoggedIn: boolean = false;
+  isDarkTheme: boolean = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private themeService: ThemeService) {}
+
   ngOnInit(): void {
     this.isLoggedIn = this.authService.isAuth;
+    this.isDarkTheme = localStorage.getItem('dark-theme-enabled') === 'true';
+
+    this.themeService.themeChanged.subscribe((val) => {
+      this.isDarkTheme = val;
+    });
   }
 
   logout() {
     this.authService.logout();
     this.isLoggedIn = false;
+  }
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
   }
 }
